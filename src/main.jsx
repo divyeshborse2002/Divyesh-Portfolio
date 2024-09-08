01d6ada711bf8cfeb67108654ec95aefd35769aa
divyeshborse2002 <divyeshborse2002@gmail.com>
2024-09-08 11:13:21 +0530

    work

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>,
+
 )
